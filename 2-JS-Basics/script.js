b0809ffa46eee604fe7244e454001109b7d3b80f
nsyawali12 <nsyawali12@gmail.com>
2019-08-07 06:11:29 +0700

    basic operators

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -52,3 +52,34 @@ alert(firstName + ' is a' + age + ' year old ' + job + '. is he married? ' + isM
 // Asking a user input
 var lastName = prompt(' What is his last Name? ');
 console.log(firstName + ' ' + lastName);
+
+/***********************
+ * Basic Operators
+ */
+
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math Operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical Operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+//typeof Operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+
+var x;
+console.log(typeof x);
