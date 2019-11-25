a33fddccc24ea6c9448a310fff75d9494c611ecc
nsyawali12 <nsyawali12@gmail.com>
2019-11-25 13:55:40 +0700

    boolean logic

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -188,3 +188,20 @@ if (BMIMark > BMIJohn) {
 } else {
     console.log('John\' BMI is higher than Mark.');
 }
+
+/********************
+ *   BOOLEAN LOGIC
+ */
+
+var firstName = 'John';
+var age = 20;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man. ');
+} else {
+    console.log(firstName + ' is a man. ');
+}
